359366b3c7fc6e3872433f8256ebc1ec6b1e78fd
“Julian” <j.makhoul1995@gmail.com>
2022-02-06 11:20:05 +0200

    homepage-ready

---
+++ src/components/cards/cards.js
@@ -0,0 +1,35 @@
+import ReactDOM from "react-dom";
+import React, { useState } from "react";
+import ReactCardFlip from "react-card-flip";
+import "./style.css";
+
+const CardFlip = ({ title, description, author }) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
+  return (
+    <div className="flipCardContainer">
+      <ReactCardFlip
+        isFlipped={isFlipped}
+        flipDirection="horizontal"
+        containerStyle={{ height: "100%" }}
+      >
+        <div className="flipCardFront">
+          <img src="book.png" alt="" width="150px" height="150px" />
+          <h3>{title} </h3>
+          <button onClick={handleClick}>Click to flip</button>
+        </div>
+
+        <div className="flipCardBack">
+          <h3>{author} </h3>
+          <p>{description} </p>
+          <button onClick={handleClick}>Click to flip</button>
+        </div>
+      </ReactCardFlip>
+    </div>
+  );
+};
+
+export default CardFlip;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
+import Cards from "../../components/cards/cards";
+
 const Home = () => {
+  const APIKEY = process.env.REACT_APP_APIKEY;
+  const [topselling, setTopselling] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=${APIKEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => setTopselling(data.results));
+  }, []);
+  console.log(topselling);
   return (
     <div className="homeContainer">
       <h1>Welcome to books store</h1>
+      <div className="cardsContainer">
+        {topselling.length ? (
+          topselling.map((book) => (
+            <Cards
+              author={book.author}
+              title={book.title}
+              description={book.description}
+            />
+          ))
+        ) : (
+          <h1>Loading ...</h1>
+        )}
+      </div>
     </div>
   );
 };
